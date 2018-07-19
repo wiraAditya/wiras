f791571d7489fd9705e1f90015e0831d718ceadd
wira aditya px <wiraaditya131@gmail.com>
2018-07-20 07:52:51 +0800

    fixing front design

---
+++ blog.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<!-- bootstap -->
+	<link rel="stylesheet" href="assets/dist/bootstrap/dist/css/bootstrap.min.css">
+	<!-- custom -->
+	<link rel="stylesheet" href="assets/css/custom.css">
+</head>
+<body class="bg-grey ">
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100 img-carousel" src="assets/img/1.jpg" alt="First slide">
+	      <div class="carousel-caption ">
+	      	<div class="textCapt">
+			    <h1>Lorem ipsum dolor sit amet, consectetuer.</h1>
+			    <a href="" class="btn btn-white-bordered py-2 px-4 my-5">Read</a>
+	      	</div>
+		  </div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100 img-carousel" src="assets/img/2.jpg" alt="First slide">
+	      <div class="carousel-caption ">
+	      	<div class="textCapt">
+			    <h1>Lorem ipsum dolor sit amet, consectetuer.</h1>
+			    <a href="" class="btn btn-white-bordered py-2 px-4 my-5">Read</a>
+	      	</div>
+		  </div>
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100 img-carousel" src="assets/img/3.jpg" alt="First slide">
+	      <div class="carousel-caption ">
+	      	<div class="textCapt">
+			    <h1>Lorem ipsum dolor sit amet, consectetuer.</h1>
+			    <a href="" class="btn btn-white-bordered py-2 px-4 my-5">Read</a>
+	      	</div>
+		  </div>
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Previous</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Next</span>
+	  </a>
+	</div>
+</body>
+</html>
+<script src="assets/dist/jquery/jquery-3.3.1.min.js"></script>
+<script src="assets/dist/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="assets/dist/popper/popper.min.js"></script>
